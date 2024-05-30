1d9a812ed87861ce220eca8f35f5f51c834f1f01
lwj <jg15575446613@163.com>
2024-05-30 21:59:18 +0800

    learn

--- module4/sole-and-ankle-main/src/components/ShoeCard/ShoeCard.js
+++ module4/sole-and-ankle-main/src/components/ShoeCard/ShoeCard.js
@@ -36,14 +36,24 @@ const ShoeCard = ({
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
+          {variant === 'new-release' && <NewFlag>Just Released!</NewFlag>}
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price style={
+            {
+              '--color': variant === 'on-sale' ? COLORS.gray['700'] : '',
+              '--text-decoration': variant === 'on-sale' ? 'line-through' : ''
+            }
+          }>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          {
+            variant === 'on-sale' ? <SalePrice>{formatPrice(salePrice)}</SalePrice> : undefined
+          } 
         </Row>
       </Wrapper>
     </Link>
@@ -51,6 +61,8 @@ const ShoeCard = ({
 };
 
 const Link = styled.a`
+  flex: 1;
+  flex-basis: 340px;
   text-decoration: none;
   color: inherit;
 `;
@@ -60,7 +72,6 @@ const Wrapper = styled.article``;
 const ImageWrapper = styled.div`
   position: relative;
   display: flex;
-  width: 340px;
 `;
 
 const Image = styled.img`
@@ -71,6 +82,8 @@ const Image = styled.img`
 const Row = styled.div`
   font-size: 1rem;
   display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Name = styled.h3`
@@ -80,6 +93,8 @@ const Name = styled.h3`
 
 const Price = styled.span`
   margin-left: auto;
+  color: var(--color);
+  text-decoration: var(--text-decoration);
 `;
 
 const ColorInfo = styled.p`
@@ -90,5 +105,24 @@ const SalePrice = styled.span`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.primary};
 `;
+const Flag = styled.div`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  background: red;
+  height: 32px;
+  line-height: 32px;
+  padding: 0 10px;
+  font-size ${14 / 18}rem;
+  font-weight: ${WEIGHTS.bold};
+  color: white;
+  border-radius: 4px;
+`
+const SaleFlag = styled(Flag)`
+  background-color: ${COLORS.primary};
+`
+const NewFlag = styled(Flag)`
+  background-color: ${COLORS.secondary};
+`
 
 export default ShoeCard;
